4e60507ec12c2aeb62389c18c43019d577a101e5
unknown <ceci.ga003@gmail.com>
2024-01-21 22:51:03 -0600

    added questions array in js file

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        question: "Commonly used data types DO not Include:",
+        options: [ "1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "2. booleans", 
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _______.",
+        options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+        answer: "3. parenthesis", 
+    },
+    {
+        question: "Arrays in Javascript can be used to store _______.",
+        options: ["1. numbers of strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "4. all of the above",
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        answer: "3. quotes",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        answer: "4. console.log",
+    },    
+]
+
