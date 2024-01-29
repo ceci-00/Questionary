ca66749b495bff125e2b3f07ef1962dec45478ed
unknown <ceci.ga003@gmail.com>
2024-01-28 18:44:59 -0600

    loaded randomized questions on js and created questionList array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,33 @@
 var startButton = document.getElementById('start-btn')
+var questionEl = document.getElementById('question')
+var optionE1 = document.getElementById('option')
+var shuffleQuestions, currentQuestion
 
 
 startButton.addEventListener('click', startQuiz)
 
 function startQuiz() {
-    console.log('started');
-    document.getElementById('home').style.display='none';
+    console.log('started')
+    document.getElementById('home').style.display = 'none'
+    shuffleQuestions = questionList.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    setNextQuestion()
 }
 
+function setNextQuestion() {
+    showQuestion(shuffleQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionEl.innerText = question.question
 
 
+
+}
+
+function selectAnswer(e) {
+
+}
 
 //r questionE1 = document.getElementById("question");
 //r optionId = document.getElementById("option");
@@ -17,33 +35,33 @@ function startQuiz() {
 //nction startQuiz(event) {
 //  event.preventDefault;
 //
-//r questionList = [
-//  {
-//      question: "Commonly used data types DO not Include:",
-//      options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-//      answer: "2. booleans",
-//  },
-//  {
-//      question: "The condition in an if/else statement is enclosed with _______.",
-//      options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
-//      answer: "3. parenthesis",
-//  },
-//  {
-//      question: "Arrays in Javascript can be used to store _______.",
-//      options: ["1. numbers of strings", "2. other arrays", "3. booleans", "4. all of the above"],
-//      answer: "4. all of the above",
-//  },
-//  {
-//      question: "String values must be enclosed within _______ when being assigned to variables.",
-//      options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
-//      answer: "3. quotes",
-//  },
-//  {
-//      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-//      options: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
-//      answer: "4. console.log",
-//  },
-//
+var questionList = [
+    {
+        question: "Commonly used data types DO not Include:",
+        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        correct: "2. booleans",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _______.",
+        options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+        correct: "3. parenthesis",
+    },
+    {
+        question: "Arrays in Javascript can be used to store _______.",
+        options: ["1. numbers of strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correct: "4. all of the above",
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        correct: "3. quotes",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        correct: "4. console.log",
+    },
+];
 
 // randomize selection of questions
 
