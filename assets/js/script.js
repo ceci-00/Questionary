835510afda97eeb21c93be377b11ceb6335bccf2
ceci-00 <ceci.ga003@gmail.com>
2024-02-18 11:35:11 -0600

    created showQuestion function and correctly linked it to startQuiz function to display questions and answers on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,78 @@
 var startButton = document.getElementById('start-btn')
+console.dir(startButton)
 var questionEl = document.getElementById('question')
-var optionE1 = document.getElementById('option')
-var shuffleQuestions, currentQuestion
-
-
+var answerButtons = document.getElementsByClassName('btn')
+var currentQuestionIndex = 0
+function logStart() {
+    console.log("=====================================")
+}
+function logEnd() {
+    console.log("*****************************************")
+}
 startButton.addEventListener('click', startQuiz)
 
 function startQuiz() {
     console.log('started')
+    // hides home page and starts quiz with 1st question
     document.getElementById('home').style.display = 'none'
-    shuffleQuestions = questionList.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    setNextQuestion()
-}
+    document.getElementById('quiz-container').style.display = 'block'
 
+    // questionEl.innerText(shuffleQuestions)
+    // console.log(shuffleQuestions)
+    // setNextQuestion()
+    showQuestion()
+}
+// load next question
 function setNextQuestion() {
-    showQuestion(shuffleQuestions[currentQuestionIndex])
+    logStart()
+    console.log("Setting next question")
+    // if I'm not at the end of my array -> 
+    //currentQuestionIndex ++ 
+    // showQuestion()
+    //else {
+    // endQuiz()
+    // }
+    logEnd()
 }
 
-function showQuestion(question) {
-    questionEl.innerText = question.question
+//timer you'll need to check if we've reached the last question or we ran out of time -> end the Timer and call endQuiz()
 
+function showQuestion() {
+    console.log("Showing question function running");
 
+    var currentQuestion = questionList[currentQuestionIndex];
+    console.log("SHow Question: ", currentQuestion);
+    // randomize selection of questions
 
+    questionEl.innerText = currentQuestion.question
+    currentQuestion.options.forEach((option, index) => {
+        answerButtons[index].innerText = option;
+        answerButtons[index].addEventListener('click', selectAnswer)
+    })
 }
 
 function selectAnswer(e) {
+    console.log("SELECTED")
+    console.log(e);
+    var selectedButton = e.target
+    console.log(selectedButton)
+    var selectedAnswer = selectedButton.innerText
+    console.log(selectedAnswer)
+    var currentQuestion = questionList[currentQuestionIndex];
+    console.log(currentQuestion)
+    var correctAnswer = currentQuestion.correct
+    console.log(correctAnswer)
+    //if it's correct do correct stuff
+    //else do incorrect stuff
+
+    setNextQuestion()
 
 }
 
-//r questionE1 = document.getElementById("question");
-//r optionId = document.getElementById("option");
+//varr questionE1 = document.getElementById("question");
+//var optionId = document.getElementById("option");
 //
-//nction startQuiz(event) {
+//function startQuiz(event) {
 //  event.preventDefault;
 //
 var questionList = [
@@ -63,8 +103,6 @@ var questionList = [
     },
 ];
 
-// randomize selection of questions
-
 //populate answers on page
 
 //function to check for correct answer
@@ -75,8 +113,6 @@ var questionList = [
 
 //timer
 
-//load next question
-
 // all done screen - enter initials and submit to view high scores
 
 // display scores
@@ -84,9 +120,6 @@ var questionList = [
 // saves submit form info into local storage
 
 // gathers data from previous scores and sorts highest to lowest
-
-// questions html
-
 
 
 
